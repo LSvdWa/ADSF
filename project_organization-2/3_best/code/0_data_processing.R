7b9e7c77db54fb1b15b916579c8d12bb3cdc0a7d
YenotyEtoKruto <frisoharff@gmail.com>
2025-11-13 12:53:04 +0100

    preprocessing

--- project_organization-2/3_best/code/0_data_processing.R
+++ project_organization-2/3_best/code/0_data_processing.R
@@ -1,3 +1,12 @@
-datacsv = read.csv("./data/raw/tested.csv", header = TRUE)
+# Load .CSV and save as .RDS
+datacsv <- read.csv("./data/raw/tested.csv", header = TRUE)
 saveRDS(datacsv, "./data/raw/dataset.rds")
 
+# Preprocessing
+dataRDS <- readRDS("./data/raw/dataset.rds")
+dataRDS <- na.omit(dataRDS) # removes NA from important columns
+saveRDS(dataRDS, "./data/processed/dataset.rds")
+dataset <- readRDS("./data/processed/dataset.rds")
+summary(dataset)
+
+# PassengerId Survived Pclass Name Sex Age SibSp Parch Ticket Fare Cabin Embarked
