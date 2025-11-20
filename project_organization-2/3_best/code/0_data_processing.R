8aaa5f421614374aba95d12def54b38a1572307b
LSvdWa <l.s.vanderwaal@students.uu.nl>
2025-11-20 09:37:46 +0100

    fix

--- project_organization-2/3_best/code/0_data_processing.R
+++ project_organization-2/3_best/code/0_data_processing.R
@@ -1,12 +1,13 @@
 # Load .CSV and save as .RDS
-datacsv <- read.csv("./data/raw/tested.csv", header = TRUE)
-saveRDS(datacsv, "./data/raw/dataset.rds")
+
+datacsv <- read.csv("./ADSF/project_organization-2/3_best/data/raw/diabetes.csv", header = TRUE)
+saveRDS(datacsv, "./ADSF/project_organization-2/3_best/data/raw/dataset.rds")
 
 # Preprocessing
-dataRDS <- readRDS("./data/raw/dataset.rds")
+dataRDS <- readRDS("./ADSF/project_organization-2/3_best/data/raw/dataset.rds")
 dataRDS <- na.omit(dataRDS) # removes NA from important columns
-saveRDS(dataRDS, "./data/processed/dataset.rds")
-dataset <- readRDS("./data/processed/dataset.rds")
+saveRDS(dataRDS, "./ADSF/project_organization-2/3_best/data/processed/dataset.rds")
+dataset <- readRDS("./ADSF/project_organization-2/3_best/data/processed/dataset.rds")
 summary(dataset)
 
 # PassengerId Survived Pclass Name Sex Age SibSp Parch Ticket Fare Cabin Embarked
