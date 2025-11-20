ca46d1e0c4c0313c608192c6046eba1b8739580a
LSvdWa <l.s.vanderwaal@students.uu.nl>
2025-11-20 10:12:50 +0100

    Ompolen BMI

--- project_organization-2/3_best/code/0_data_processing.R
+++ project_organization-2/3_best/code/0_data_processing.R
@@ -1,13 +1,20 @@
 # Load .CSV and save as .RDS
 
-datacsv <- read.csv("./ADSF/project_organization-2/3_best/data/raw/diabetes.csv", header = TRUE)
-saveRDS(datacsv, "./ADSF/project_organization-2/3_best/data/raw/dataset.rds")
+datacsv <- read.csv("./data/raw/diabetes.csv", header = TRUE)
+saveRDS(datacsv, "./data/raw/dataset.rds")
 
 # Preprocessing
-dataRDS <- readRDS("./ADSF/project_organization-2/3_best/data/raw/dataset.rds")
+dataRDS <- readRDS("./data/raw/dataset.rds")
 dataRDS <- na.omit(dataRDS) # removes NA from important columns
-saveRDS(dataRDS, "./ADSF/project_organization-2/3_best/data/processed/dataset.rds")
-dataset <- readRDS("./ADSF/project_organization-2/3_best/data/processed/dataset.rds")
+saveRDS(dataRDS, "./data/processed/dataset.rds")
+dataset <- readRDS("./data/processed/dataset.rds")
+
+# Recoding
+dataRDS$BMI[dataRDS$BMI <= 18.5 ] <- 1
+dataRDS$BMI[dataRDS$BMI > 18.5 & dataRDS$BMI <= 25  ] <- 2
+dataRDS$BMI[dataRDS$BMI > 25 & dataRDS$BMI <= 30 ] <- 3
+dataRDS$BMI[dataRDS$BMI > 30] <- 4
+
 summary(dataset)
 
 # PassengerId Survived Pclass Name Sex Age SibSp Parch Ticket Fare Cabin Embarked
